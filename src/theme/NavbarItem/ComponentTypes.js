bd8f26882c44bd01e5a01c57be5cd6ce58ca3196
VGalaxies <vgalaxies@apache.org>
2026-05-05 14:01:42 +0000

    docs: preserve locale in navbar links

--- src/theme/NavbarItem/ComponentTypes.js
+++ src/theme/NavbarItem/ComponentTypes.js
@@ -1,7 +1,9 @@
 import ComponentTypes from '@theme-original/NavbarItem/ComponentTypes';
 import LanguageSwitcherNavbarItem from '@site/src/components/LanguageSwitcherNavbarItem';
+import LocaleAwareNavbarItem from '@site/src/components/LocaleAwareNavbarItem';
 
 export default {
   ...ComponentTypes,
   'custom-languageSwitcher': LanguageSwitcherNavbarItem,
+  'custom-localeAwareLink': LocaleAwareNavbarItem,
 };
